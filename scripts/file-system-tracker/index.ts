b57f07e8cf6c66ca2b07dffe9cdc143a270e3e3b
MZBCodes <96595094+MZBCodes@users.noreply.github.com>
2024-07-10 17:04:14 -0400

    Added file system logging functionality
    Saves and appends to files stored in your chosen directory under the alias of "{languageName}Line.csv"
    
    You can change the aliases and the languages you want to track as needed

--- scripts/file-system-tracker/index.ts
+++ scripts/file-system-tracker/index.ts
@@ -1,12 +1,39 @@
-import { readdirSync, Stats } from "fs";
+import { readdirSync, Stats, write, writeFile } from "fs";
+
+
+type LanguageEntry = [string, number];
+type ExtensionType = {
+    [key: string]: string;
+  };
 
 const fs = require('fs');
-const rootPath = "C:\\Path_To_Your_Code_Directory"
-const exclusions = ["public", "build", "node_modules", "__pycache__", ".git", "debug", ".vscode",]
-const codeExtensions = [".bat", ".c", ".clj", ".cljs", ".coffee", ".cpp", ".cs", ".css", ".dart", ".erl", ".ex", ".exs", ".f", ".f90", ".f95", ".go", ".groovy", ".h", ".hpp", ".hs", ".html", ".java", ".jl", ".js", ".jsx", ".kt", ".less", ".lisp", ".lua", ".m", ".m4", ".mjs", ".nim", ".pas", ".perl", ".php", ".pl", ".pp", ".ps1", ".py", ".r", ".rkt", ".rs", ".sass", ".scala", ".scss", ".sh", ".sql", ".swift", ".tcl", ".ts", ".tsx", ".v", ".vhdl", ".vue", ".xml", ".yaml", ".yml"];
-const codeData = new Map();
-for (let i = 0; i < codeExtensions.length; i++) {
-    codeData.set(codeExtensions[i], 0)
+const rootPath: string = "C:\\Path\\To\\Code\\Directory"
+const writePath: string = "C:\\Path\\To\\Stats\\Directory"
+const exclusions: string[] = ["public", "build", "node_modules", "__pycache__", ".git", "debug", ".vscode",]
+const codeExtensions: string[] = [".bat", ".c", ".clj", ".cljs", ".cpp", ".cs", ".css", ".go", ".hs", ".html", ".java", ".js", ".jsx", ".lua",".mjs", ".nim", ".pas", ".perl", ".php", ".pl", ".ps1", ".py", ".r", ".rkt", ".rs", ".sass", ".scala", ".scss", ".sh", ".sql", ".swift", ".tcl", ".ts", ".tsx", ".vhdl", ".xml", ".yaml", ".yml"];
+const extensionMap: ExtensionType = {
+    ".js": "javascript",
+    ".css": "css",
+    ".py": "python",
+    ".html": "html",
+    ".ps1": "powershell",
+    ".ts": "typescript",
+    ".c": "c",
+    ".tsx": "typescriptx",
+    ".go": "golang",
+    ".rs": "rust",
+    ".cpp": "cplusplus",
+}
+
+const omitTypescript = (file: string, allFiles: string[]): boolean => {
+    let title = file.split(".")[0];//get the title of the js file
+    for (let i = 0; i < allFiles.length; i++){
+        let tempTitle = allFiles[i].split('.')[0]
+        if ((tempTitle === title) && (allFiles[i].split('.')[1] == "ts")){//if TS file shares the same name as a javascript file then don't include the JS file.
+            return true;//don't include JS files that have the same name as JS files
+        }
+    }
+    return false;
 }
 
 const filterCodeFiles = (files: string[]): string[] => {
@@ -18,7 +45,12 @@ const filterCodeFiles = (files: string[]): string[] => {
         if (fileExt) {
             if (!codeExtensions.includes("." + fileExt)) {
                 nonCodeFiles.push(fileName)
+            } else if (fileExt == "js") {
+                if (omitTypescript(fileName, files)){
+                    nonCodeFiles.push(fileName)
+                }
             }
+            
         } else {
             nonCodeFiles.push(fileName)
         }
@@ -112,8 +144,58 @@ const getTotalLines = (files: string[]): number => {
     }
     return sum
 }
+
+const writeFiles = (data: Array<LanguageEntry>): void => {
+    const headers = ["Number of Lines", "Date Logged", "Difference"]
+    let i = 0;
+    let tempData: LanguageEntry = data[i]//current language sorted by most amount of lines
+    let numLines: number = tempData[1]//get the number of lines of chosen language
+
+    let currDate: Date = new Date();
+    let formattedDate: string = new Intl.DateTimeFormat('en-US').format(currDate).replace(/\//g, '-')//get the current date in mm-dd-yyyy format
+
+    while (numLines != 0){
+        let entrydata: string[] = [tempData[1].toString(), formattedDate]//num lines => date Logged => difference
+        let fileName: string = extensionMap[tempData[0]] + "Lines.csv" //name of the file based off the extension (".js" => "javascriptLines")
+        let fullpath: string = writePath + "\\" + fileName  //get the absolute path of the new file...
+        let difference: string = "" 
+
+        if (fs.existsSync(fullpath)){//if the file already exists, append to it and calculat ethe difference
+            let tempFileData: string[] = fs.readFileSync(fullpath, "utf-8").split("\n")//read in the file to calculate the difference
+            if (tempFileData.length > 1){
+                let lastEntry: string = tempFileData[tempFileData.length - 2]//account for empty character at the end
+                let lastNumber = lastEntry.split(',')[0]
+                console.log(lastEntry, lastNumber)
+                difference = (parseInt(entrydata[0]) - parseInt(lastNumber)).toString()
+            } else {
+                difference = entrydata[0]
+            }
+            entrydata.push(difference)
+            fs.appendFileSync(fullpath, entrydata.join(",") + "\n");
+            console.log('String appended to the existing file.');
+        } else {//if the file doesn't exist, write the headers and set the difference to the number of lines
+            difference = entrydata[0]
+            entrydata.push(difference)
+            fs.writeFileSync(fullpath, headers.join(",") + "\n");//write the CSV headers first
+            fs.appendFileSync(fullpath, entrydata.join(",") + "\n");//write the data
+            console.log('File written at: ', fullpath);
+        }
+        i++;
+        tempData = data[i]
+        numLines = tempData[1]
+    }
+
+}
+
+const codeData = new Map();//initialize mapping of languages and lines
+for (let i = 0; i < codeExtensions.length; i++) {
+    codeData.set(codeExtensions[i], 0)
+}
+
 const files: string[] = getAllFiles(rootPath)
 const numLines: number = getTotalLines(files)
+const sortedArray = Array.from(codeData.entries()).sort((a, b) => b[1] - a[1])
 console.log("Total Lines: ", numLines)
-console.log(Array.from(codeData.entries()).sort((a, b) => b[1] - a[1]))
-console.log("test")
+console.log(sortedArray)
+
+writeFiles(sortedArray)
